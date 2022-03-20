ad1d7b6b5f1aeac4543fc3ead72f6c0d85b6fb61
jkling2 <jkling2@users.noreply.github.com>
2022-03-20 18:07:40 +0000

    deploy: jkling2/tennis-bookings-crawl-dash@d7f93659f90d4fd24abe9c6f38bd6f5697843f99

--- assets/callback.js
+++ assets/callback.js
@@ -6,7 +6,8 @@ window.dash_clientside.clientside = {
     update_store_data_initial: function(n_intervals) {
         var today = new Date();
         var reference_day = today.toLocaleString('de-DE', {weekday:'long'});
-        var reference_day_time = today.getHours() + ":" + (parseInt(today.getMinutes() / 15) * 15);
+        var minutes = parseInt(today.getMinutes() / 15) * 15;
+        var reference_day_time = today.getHours() + ":" + ((minutes < 10 ? "0" : "") + minutes);
         return [reference_day, reference_day_time];
     },
     update_store_data: function(reference_day, reference_day_time, store_data, days_d_to_en_dic) {
